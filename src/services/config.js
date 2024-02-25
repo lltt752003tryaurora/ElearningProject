418672e69d1b01fb4aab7920928fba99fb2bc3a9
Le Long Truong Thinh <truongthinhkl1@gmail.com>
2024-02-25 19:49:50 +0700

    update

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const URL_DOMAIN = "https://elearningnew.cybersoft.edu.vn";
 const TokenCybersoft =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0OCIsIkhldEhhblN0cmluZyI6IjEwLzAyLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwNzUyMzIwMDAwMCIsIm5iZiI6MTY3OTY3NzIwMCwiZXhwIjoxNzA3NjcwODAwfQ.N-naoH9C9l_9p7kMChk45-IrJfIqEYyMlZijuzHsXsI";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlSlMgMzciLCJIZXRIYW5TdHJpbmciOiIyNi8wNS8yMDI0IiwiSGV0SGFuVGltZSI6IjE3MTY2ODE2MDAwMDAiLCJuYmYiOjE2OTg1MTI0MDAsImV4cCI6MTcxNjgyOTIwMH0.WRdYDUhhNdFlNs99AP2dpjnCOrvxtAV4BH7IvNMVa_Y";
 
 const userData = JSON.parse(localStorage.getItem("user"));
 
